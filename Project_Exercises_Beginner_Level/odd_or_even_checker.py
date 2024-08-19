58c7ad629939e9a051588bd5b488ebf462e37e35
Chels-M <102589616+Chels-M@users.noreply.github.com>
2024-08-19 20:52:42 +0100

    added odd_or_even_checker.py

---
+++ Project_Exercises_Beginner_Level/odd_or_even_checker.py
@@ -0,0 +1,10 @@
+# program checks whether given number is odd or even
+
+value = float(input("Please type a numeric value: "))
+
+if value % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
+
